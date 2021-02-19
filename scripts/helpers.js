5731f24f6c3222a2d48a755eb73b706f54302041
mustafadincay <mustafadincay@gmail.com>
2021-02-20 01:24:40 +0300

    tekrar eden kısımlar yapılacak

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,7 +1,54 @@
+import data from "./data.js";
 export const searchMovieByTitle = (movie, searchValue) => {
-    return movie.title.toLowerCase().indexOf(searchValue.toLowerCase()) > -1;
-}
+  return movie.title.toLowerCase().indexOf(searchValue.toLowerCase()) > -1;
+};
 
 export const makeBgActive = (movie) => {
-    document.querySelector(`tr[data-id='${movie.id}']`).style.background = "#d7f0f7";
-}
+  document.querySelector(`tr[data-id='${movie.id}']`).style.background =
+    "#d7f0f7";
+};
+
+//radiobuttons oluşturma
+export const createRadioEl = (type, name, id, value) => {
+  return `  
+  <div class="form-check">
+  <input class="form-check-input" type="${type}" name="${name}" id="${name}${id}" value="${value}">
+  <label class="form-check-label"> ${value} </label>
+  </div>`;
+};
+
+///checkbox oluşturma
+export const createCheckEl = (type, name, id, value) => {
+  return `
+  <div class="form-check">
+    <input class="form-check-input" type="${type}" name="${name}" id="${name}${id} "value="${value}">
+      <label class="form-check-label">
+      ${value}
+      </label>
+  </div>`;
+};
+//tekrar etmeyen yılları bulmak için yapılan fonksiyon
+export const findMovie = () => {
+  let arr = [];
+  let uniq = [];
+  data.map((movie) => {
+    arr.push(movie.year);
+    uniq = [...new Set(arr)];
+  });
+  return {
+    uniq,
+  };
+};
+
+//tekrar etmeyen genreleri bulmak için yapılan fonksiyon
+export const findGenre = () => {
+  let arr = [];
+  let uniq = [];
+  data.map((movie) => {
+    arr.push(movie.genre);
+    uniq = [...new Set(arr)];
+  });
+  return {
+    uniq,
+  };
+};
--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,156 @@
 import data from "./data.js";
-import {searchMovieByTitle, makeBgActive} from "./helpers.js";
+import {
+  searchMovieByTitle,
+  makeBgActive,
+  createRadioEl,
+  findMovie,
+  findGenre,
+  createCheckEl,
+} from "./helpers.js";
 
 class MoviesApp {
-    constructor(options) {
-        const {root, searchInput, searchForm, yearHandler, yearSubmitter} = options;
-        this.$tableEl = document.getElementById(root);
-        this.$tbodyEl = this.$tableEl.querySelector("tbody");
+  constructor(options) {
+    const {
+      root,
+      searchInput,
+      searchForm,
+      yearHandler,
+      yearSubmitter,
+      movieYears,
+      movieGenres,
+      form_check,
+      genreSubmitter,
+      genreHandler,
+    } = options;
+    this.$tableEl = document.getElementById(root);
+    this.$tbodyEl = this.$tableEl.querySelector("tbody");
+    this.$searchInput = document.getElementById(searchInput);
+    this.$searchForm = document.getElementById(searchForm);
+    this.yearHandler = yearHandler;
+    this.$yearSubmitter = document.getElementById(yearSubmitter);
+    this.genreHandler = genreHandler;
+    this.$genreSubmitter = document.getElementById(genreSubmitter);
+    this.$movieYears = document.getElementById(movieYears);
+    this.$movieGenres = document.getElementById(movieGenres);
+    this.$form_check = document.getElementById(form_check);
+  }
 
-        this.$searchInput = document.getElementById(searchInput);
-        this.$searchForm   = document.getElementById(searchForm);
-        this.yearHandler = yearHandler;
-        this.$yearSubmitter = document.getElementById(yearSubmitter);
-    }
+  createMovieEl(movie) {
+    const { image, title, genre, year, id } = movie;
+    return `<tr data-id="${id}"><td><img src="${image}"></td><td>${title}</td><td>${genre}</td><td>${year}</td></tr>`;
+  }
 
-    createMovieEl(movie){
-        const {image, title, genre, year,id} = movie;
-        return `<tr data-id="${id}"><td><img src="${image}"></td><td>${title}</td><td>${genre}</td><td>${year}</td></tr>`
-    }
-
-    fillTable(){
-        /* const moviesHTML = data.reduce((acc, cur) => {
+  fillTable() {
+    /* const moviesHTML = data.reduce((acc, cur) => {
             return acc + this.createMovieEl(cur);
         }, "");*/
-        const moviesArr = data.map((movie) => {
-           return this.createMovieEl(movie)
-        }).join("");
-        this.$tbodyEl.innerHTML = moviesArr;
-    }
+    const moviesArr = data
+      .map((movie) => {
+        return this.createMovieEl(movie);
+      })
+      .join("");
+    this.$tbodyEl.innerHTML = moviesArr;
+  }
 
-    reset(){
-        this.$tbodyEl.querySelectorAll("tr").forEach((item) => {
-            item.style.background = "transparent";
+  reset() {
+    this.$tbodyEl.querySelectorAll("tr").forEach((item) => {
+      item.style.background = "transparent";
+    });
+  }
+
+  handleSearch() {
+    this.$searchForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      this.reset();
+      const searchValue = this.$searchInput.value;
+      data
+        .filter((movie) => {
+          return searchMovieByTitle(movie, searchValue);
         })
+        .forEach(makeBgActive);
+      this.$searchInput.value = "";
+    });
+  }
+
+  fillYear() {
+    const temp = findMovie(); //burdan benzersiz yıllar geliyor
+    for (let i = 0; i < temp.uniq.length; i++) {
+      let radioBtns = createRadioEl(
+        "radio",
+        `${data.year}`,
+        `${data.year}${data.id}`,
+        temp.uniq[i]
+      ); //radio button oluşturuluyor
+      let tempHTML = this.$movieYears; //DOM kısmı tutuluyor
+      tempHTML.innerHTML += radioBtns;
     }
+  }
 
-
-    handleSearch(){
-        this.$searchForm.addEventListener("submit", (event) => {
-            event.preventDefault();
-            this.reset();
-            const searchValue = this.$searchInput.value;
-            const matchedMovies = data.filter((movie) => {
-                return searchMovieByTitle(movie, searchValue);
-            }).forEach(makeBgActive)
-
-        });
+  fillGenre() {
+    const temp = findGenre(); //burdan benzersiz genreler geliyor
+    for (let i = 0; i < temp.uniq.length; i++) {
+      let genreBtns = createCheckEl(
+        "checkbox",
+        `${data.genre}`,
+        `${data.genre}${data.id}`,
+        temp.uniq[i]
+      ); //checkbox oluşturuluyor
+      let tempHTML = this.$movieGenres; //DOM kısmı tutuluyor
+      tempHTML.innerHTML += genreBtns;
     }
+  }
 
-    handleYearFilter(){
-        this.$yearSubmitter.addEventListener("click", () => {
-            this.reset();
-            const selectedYear = document.querySelector(`input[name='${this.yearHandler}']:checked`).value
-            const matchedMovies = data.filter((movie) => {
-                return movie.year === selectedYear;
-            }).forEach(makeBgActive)
-        });
-    }
+  newHandleYearFilter() {
+    const temp = findMovie(); ////uniq yıllar geldi
+    this.$yearSubmitter.addEventListener("click", () => {
+      this.reset();
+      const selectedYear = document.querySelector(
+        `input[name='${this.yearHandler}']:checked`
+      ).value;
+      data
+        .filter((movie) => {
+          return movie.year === selectedYear;
+        })
+        .forEach(makeBgActive);
+    });
+  }
 
-    init(){
-        this.fillTable();
-        this.handleSearch();
-        this.handleYearFilter();
-    }
+  handleGenreFilter() {
+    const temp = findGenre(); //uniq genreler geldi
+    this.$genreSubmitter.addEventListener("click", () => {
+      this.reset();
+      const selectedGenre = Array.from(
+        document.querySelectorAll(`input[name='${this.genreHandler}']:checked`)
+      ).map((selected) => selected.value);
+      data
+        .filter((movie) => selectedGenre.some((genre) => genre == movie.genre))
+        .forEach(makeBgActive);
+    });
+  }
+
+  init() {
+    this.fillTable();
+    this.handleSearch();
+    this.newHandleYearFilter();
+    //findMovie();
+    //findGenre();
+    this.fillYear();
+    this.fillGenre();
+    this.handleGenreFilter();
+  }
 }
 
 let myMoviesApp = new MoviesApp({
-    root: "movies-table",
-    searchInput: "searchInput",
-    searchForm: "searchForm",
-    yearHandler: "year",
-    yearSubmitter: "yearSubmitter"
+  root: "movies-table",
+  searchInput: "searchInput",
+  searchForm: "searchForm",
+  yearHandler: `${data.year}`,
+  yearSubmitter: "yearSubmitter",
+  movieYears: "movieYears",
+  movieGenres: "movieGenres",
+  form_check: "form_check",
+  genreSubmitter: "genreSubmitter",
+  genreHandler: `${data.genre}`,
 });
 
 myMoviesApp.init();
